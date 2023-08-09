f788925dbe0cbb822bf269c80b30452a4236d88a
Aish-14-Aishu <141447188+Aish-14-Aishu@users.noreply.github.com>
2023-08-09 22:03:23 +0530

    script.js

--- To-Do List/script.js
+++ To-Do List/script.js
@@ -1,1 +1,31 @@
+let result = document.getElementById('result');
+
+function appendToResult(value) {
+    result.value += value;
+}
+
+function appendOperator(operator) {
+    if (result.value.length > 0) {
+        const lastChar = result.value.slice(-1);
+        if (!isNaN(lastChar) || lastChar === '.') {
+            result.value += operator;
+        }
+    }
+}
+
+function calculate() {
+    try {
+        result.value = eval(result.value);
+    } catch (error) {
+        result.value = 'Error';
+    }
+}
+
+function clearResult() {
+    result.value = '';
+}
+
+function deleteLast() {
+    result.value = result.value.slice(0, -1);
+}
 
